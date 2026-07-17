4273eb4445c0c776a71d95d8ab91f98c19d9b197
Piero Rospigliosi <pierinho13@gmail.com>
2026-07-17 13:50:57 +0200

    new formula sha

--- Formula/kubectl-peek.rb
+++ Formula/kubectl-peek.rb
@@ -2,7 +2,7 @@ class KubectlPeek < Formula
   desc "Interactive CLI for browsing and decoding Kubernetes Secrets"
   homepage "https://github.com/pierinho13/kubectl-peek"
   url "https://github.com/pierinho13/kubectl-peek/releases/download/v0.1.0/kubectl-peek_Darwin_arm64.tar.gz"
-  sha256 "cec3fa7999513c19001d9c2196846b87030e040ed4b03cb9d714e381c142ab2b"
+  sha256 "840ba8605c3ed610fbd57c3f0781382a2eb845f2aca19dbda2b3bbf5d853a88b"
   license "MIT"
 
   depends_on arch: :arm64
